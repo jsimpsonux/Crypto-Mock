4db568819dae29cb4801c1fff328adf8b2e0cd63
TachiNomad <denscuber@gmail.com>
2024-03-15 18:24:41 +0000

    unfinished display for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Cryptodisplay from './components/cryptodisplaying'
+import Cryptoinfo from './cryptoinfo.json'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [crypto] = useState(Cryptoinfo);
+  
   return (
+
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -29,7 +33,18 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+
+
+
+
   )
+
+
+
+
+
+
+
 }
 
 export default App
