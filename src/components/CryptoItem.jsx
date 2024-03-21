580231ecae3279250a6385c44b78f69eb62c58f9
foroughgoudarzi <foroughg@yahoo.com>
2024-03-21 17:39:49 +0000

    add functionality to buy button

--- src/components/CryptoItem.jsx
+++ src/components/CryptoItem.jsx
@@ -1,10 +1,25 @@
 import Logo from './Logo'
 import Price from './Price'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 
 function CryptoItem(props) {
   
+  const [itemToBuy, setItemToBuy]= useState();
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = (event) => {
+    setItemToBuy(event.target.name);
+    
+   console.log("itemToBuy",itemToBuy);
+   setShow(true);
+  }
+
+  
 return (
     <>
       <div className="container-fluid d-flex flex-row w-100 border rounded-3 p-3 m-3 border-black bg-light border-2 align-items-center">
@@ -17,11 +32,31 @@ return (
       </div>
 
       <div className='col-2' >
-        <button className='btn btn-success w-75 p-0 pt-2 pb-2' type="button" name={props.abr}>Buy</button>
+        <Button variant="primary" name={props.abr} onClick={handleShow}>
+          Buy
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Buying {itemToBuy}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><input className="form-control form-control-lg" type="text" placeholder="Please enter the number of coins to buy"></input></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.handleBuy}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     
+     
+     
       </div>
 
       <div className='col-2'>
-        <button  className='btn btn-danger w-75 p-0 pt-2 pb-2' type="button" onClick={props.handleRemove} name={props.abr} >Remove</button>
+        <button  className='btn btn-danger w-75 p-0 pt-2 pb-2' type="button" onClick={props.handleRemove} name={props.abr}>Remove</button>
       </div>
 
       </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+// import Basket from '../components/PortfolioComponents';
 import { Typography } from '@mui/material';
 
 const Portfolio = () => {
   return (
     <Typography variant="h6" component="div">
-      This is Portfolio page
+      {/* <Basket /> */}
     </Typography>
   );
 };
